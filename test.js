c004dba113c8c621929e3a521f8a61a10e909e40
C.T. Lin <chentsulin@gmail.com>
2015-07-10 22:02:20 +0800

    improve test

--- test.js
+++ test.js
@@ -1,8 +1,26 @@
 'use strict'
 
 var assert = require('assert')
-var createActionTypes = require('./')
+var create = require('./')
 
-it('should ', function () {
-	assert.strictEqual(createActionTypes('unicorns'), 'unicorns & rainbows')
-});
+it('should generate correct actionTypes', function () {
+  var types = create([
+    'INCREMENT_COUNTER',
+    'DECREMENT_COUNTER'
+  ])
+
+  assert.strictEqual(types.INCREMENT_COUNTER, 'INCREMENT_COUNTER')
+	assert.strictEqual(types.DECREMENT_COUNTER, 'DECREMENT_COUNTER')
+})
+
+it('should generate correct actionTypes when value repeat', function () {
+  var types = create([
+    'INCREMENT_COUNTER',
+    'INCREMENT_COUNTER',
+    'DECREMENT_COUNTER',
+    'DECREMENT_COUNTER'
+  ])
+
+  assert.strictEqual(types.INCREMENT_COUNTER, 'INCREMENT_COUNTER')
+  assert.strictEqual(types.DECREMENT_COUNTER, 'DECREMENT_COUNTER')
+})

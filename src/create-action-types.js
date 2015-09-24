3f3ccb9aefe116a62cf0f459c5a542392ebc0306
C.T. Lin <chentsulin@gmail.com>
2015-09-24 10:59:00 +0800

    throw when declare a constant twice

--- src/create-action-types.js
+++ src/create-action-types.js
@@ -1,6 +1,13 @@
+import invariant from 'invariant';
+
 
 export default function(types) {
   return types.reduce((result, key) => {
+    invariant(
+      result[key] === undefined,
+      'action type `%s` has been declared. Should not declare a constant twice.',
+      key
+    );
     result[key] = key;
     return result;
   }, {});
--- test.js
+++ test.js
@@ -4,21 +4,23 @@ import create from './';
 it('should generate correct actionTypes', () => {
   const types = create([
     'INCREMENT_COUNTER',
-    'DECREMENT_COUNTER'
+    'DECREMENT_COUNTER',
   ]);
 
   assert.strictEqual(types.INCREMENT_COUNTER, 'INCREMENT_COUNTER');
   assert.strictEqual(types.DECREMENT_COUNTER, 'DECREMENT_COUNTER');
 });
 
-it('should generate correct actionTypes when value repeat', () => {
-  const types = create([
-    'INCREMENT_COUNTER',
-    'INCREMENT_COUNTER',
-    'DECREMENT_COUNTER',
-    'DECREMENT_COUNTER'
-  ]);
-
-  assert.strictEqual(types.INCREMENT_COUNTER, 'INCREMENT_COUNTER');
-  assert.strictEqual(types.DECREMENT_COUNTER, 'DECREMENT_COUNTER');
+it('should throw when action type has been declared twice', () => {
+  try {
+    create([
+      'INCREMENT_COUNTER',
+      'INCREMENT_COUNTER',
+    ]);
+  } catch (e) {
+    assert.equal(
+      e.message,
+      'Invariant Violation: action type `INCREMENT_COUNTER` has been declared. Should not declare a constant twice.'
+    );
+  }
 });
